90e4a6606631c2674817d2cc566499796904bbac
mike19952006 <mike19952006@gmail.com>
2018-03-13 00:59:19 +0300

    Сумма от 1 до N

--- Homework10.7/Program.cs
+++ Homework10.7/Program.cs
@@ -8,8 +8,20 @@ namespace Homework10._7
 {
     class Program
     {
+        static void Sum(int x)
+        {
+            int s = 0;
+            for (int i = 1; i <= x; i++)
+            {
+                s += i;
+            }
+            Console.WriteLine(s);
+        }
+
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            Sum(n);
         }
     }
 }
